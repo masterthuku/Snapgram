92eadcc4ab58cd7d763ac32c3abc933952ba613a
masterthuku <peterthuku706@gmail.com>
2024-02-16 16:09:12 +0300

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import AuthLayout from "./_auth/AuthLayout";
-import SigninForm from "./_auth/forms/SigninForm";
-import SignupForm from "./_auth/forms/SignupForm";
-import RootLayout from "./_root/RootLayout";
-import { Home } from "./_root/pages";
-import "./globals.css";
 import { Routes, Route } from "react-router-dom";
+
+import {
+  Home,
+  Explore,
+  Saved,
+  CreatePost,
+  Profile,
+  EditPost,
+  PostDetails,
+  UpdateProfile,
+  AllUsers,
+} from "@/_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import SignupForm from "@/_auth/forms/SignupForm";
+import SigninForm from "@/_auth/forms/SigninForm";
+import { Toaster } from "@/components/ui/toaster";
+
+import "./globals.css";
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
-        {/*public routes */}
+        {/* public routes */}
         <Route element={<AuthLayout />}>
           <Route path="/sign-in" element={<SigninForm />} />
           <Route path="/sign-up" element={<SignupForm />} />
         </Route>
 
-        {/*private routes */}
+        {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
+
+      <Toaster />
     </main>
   );
 };
--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,7 +1,10 @@
 import { Outlet, Navigate } from "react-router-dom";
 
-const AuthLayout = () => {
-  const isAuthenticated = false;
+import { useUserContext } from "@/context/AuthContext";
+
+export default function AuthLayout() {
+  const { isAuthenticated } = useUserContext();
+
   return (
     <>
       {isAuthenticated ? (
@@ -11,6 +14,7 @@ const AuthLayout = () => {
           <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
+
           <img
             src="/assets/images/side-img.svg"
             alt="logo"
@@ -20,6 +24,4 @@ const AuthLayout = () => {
       )}
     </>
   );
-};
-
-export default AuthLayout;
+}
